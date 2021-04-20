542cfdefd1506f4762b773e575017887494d8be0
RangerUFO <ufownl@gmail.com>
2021-04-20 16:04:32 +0800

    Improve the card dealing process

--- first_visit_mc_prediction_blackjack.py
+++ first_visit_mc_prediction_blackjack.py
@@ -4,6 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def deal_card():
+    return random.choice(sum([[i] * 4 for i in range(1, 10)], start=[]) + [10] * 16)
+
+
 class State:
     def __init__(self, usable_ace, player_sum, dealer_card):
         self.__usable_ace = 1 if usable_ace else 0
@@ -39,7 +43,7 @@ class State:
             return (self.__usable_ace, self.__player_sum, self.__dealer_card)
 
     def hit(self):
-        player_sum = self.player_sum + random.randint(1, 10)
+        player_sum = self.player_sum + deal_card()
         if player_sum > 21:
             return (None, -1)
         else:
@@ -48,7 +52,7 @@ class State:
     def stick(self):
         dealer_sum = 11 if self.dealer_card == 1 else self.dealer_card
         while dealer_sum < 17:
-            card = random.randint(1, 10)
+            card = deal_card()
             dealer_sum += 11 if dealer_sum < 11 and card == 1 else card
         if dealer_sum > 21 or self.player_sum > dealer_sum:
             return (None, 1)
@@ -62,13 +66,13 @@ def init_episode():
     usable_ace = False
     player_sum = 0
     while player_sum < 12:
-        card = random.randint(1, 10)
+        card = deal_card()
         if player_sum < 11 and card == 1:
             usable_ace = True
             player_sum += 11
         else:
             player_sum += card
-    return [(State(usable_ace, player_sum, random.randint(1, 10)), None)]
+    return [(State(usable_ace, player_sum, deal_card()), None)]
 
 
 def train(episodes, stick_threshold):
