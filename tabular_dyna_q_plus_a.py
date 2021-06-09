c22fdbc97d3908fbe649253d07b49a40a62ab867
RangerUFO <ufownl@gmail.com>
2021-06-09 21:31:28 +0800

    Add the implementation of Exercise 8.4

--- tabular_dyna_q_plus_a.py
+++ tabular_dyna_q_plus_a.py
@@ -87,6 +87,45 @@ def dyna_q_plus(mazes, time_steps, alpha, gamma, epsilon, kappa, planning_steps)
                 maze = mazes[1]
 
 
+def altered_dyna_q_plus(mazes, time_steps, alpha, gamma, epsilon, kappa, planning_steps):
+    if mazes[0].shape != mazes[1].shape:
+        raise ValueError("Invalid mazes")
+    maze = mazes[0]
+    values = np.zeros(maze.shape + (4,))
+    model = {}
+    now = 0
+    cumulative_reward = 0
+    yield cumulative_reward
+    while True:
+        state = State(np.array(np.where(maze == 1)).transpose().tolist()[0])
+        while not state is None:
+            if state.index in model:
+                action = execute_policy(values[state.index] + np.array([kappa * math.sqrt(now - t) for _, _, t in model[state.index]]), epsilon)
+            else:
+                action = execute_policy(values[state.index], epsilon)
+            index = state.index + (action,)
+            s1, r = state.transition(maze, action)
+            cumulative_reward += r
+            yield cumulative_reward
+            values[index] += alpha * (r + (0 if s1 is None else gamma * np.max(values[s1.index])) - values[index])
+            if state.index in model:
+                model[state.index][action] = (s1, r, now)
+            else:
+                model[state.index] = [(s1, r, now) if a == action else (state, 0, 0) for a in range(4)]
+            state = s1
+            for _ in range(planning_steps):
+                s = random.choice(list(model))
+                actions = model[s]
+                a = random.randrange(0, len(actions))
+                s1, r, _ = actions[a]
+                values[s][a] += alpha * (r + (0 if s1 is None else gamma * np.max(values[s1.index])) - values[s][a])
+            now += 1
+            if now >= time_steps:
+                return
+            if now == 1000:
+                maze = mazes[1]
+
+
 class CumulativeReward:
     def __init__(self, f):
         self.__f = f
@@ -96,7 +135,7 @@ class CumulativeReward:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Tabular Dyna-Q+ (Example 8.2).")
+    parser = argparse.ArgumentParser(description="Tabular Dyna-Q+ (Example 8.2, Exercise 8.4).")
     parser.add_argument("--rounds", help="total number of running rounds (default: 30)", type=int, default=30)
     parser.add_argument("--time_steps", help="number of time steps (default: 3000)", type=int, default=3000)
     parser.add_argument("--alpha", help="constant step-size parameter of TD (default: 1.0)", type=float, default=1.0)
@@ -112,6 +151,8 @@ if __name__ == "__main__":
         plt.plot(np.mean(np.concatenate(p.map(Runner(mazes, args.time_steps, args.alpha, args.gamma, args.epsilon, args.planning_steps), [CumulativeReward(dyna_q) for _ in range(args.rounds)])), 0), label="Dyna-Q")
         print("Dyna-Q+ training...")
         plt.plot(np.mean(np.concatenate(p.map(Runner(mazes, args.time_steps, args.alpha, args.gamma, args.epsilon, args.kappa, args.planning_steps), [CumulativeReward(dyna_q_plus) for _ in range(args.rounds)])), 0), label="Dyna-Q+")
+        print("Altered Dyna-Q+ training...")
+        plt.plot(np.mean(np.concatenate(p.map(Runner(mazes, args.time_steps, args.alpha, args.gamma, args.epsilon, args.kappa, args.planning_steps), [CumulativeReward(altered_dyna_q_plus) for _ in range(args.rounds)])), 0), label="Altered Dyna-Q+")
         print("Done!")
     plt.grid(True)
     plt.legend()
