cb061860a74fba93cb4ab38968612140ed1077f0
RangerUFO <ufownl@gmail.com>
2021-08-01 01:24:04 +0800

    Add the implementation of Example 10.1

---
+++ semi_gradient_sarsa.py
@@ -0,0 +1,47 @@
+import random
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from multiprocessing import cpu_count, Pool
+from utils.parallel import Runner
+from utils.mountain_car import State, TileCoding
+
+
+def train(episodes, epsilon, basis):
+    w = np.zeros((basis.dimensions, 1))
+    for _ in range(episodes):
+        s = State()
+        a = basis.policy(s, w, epsilon)
+        steps = 0
+        while True:
+            s1, r = s.transition(a)
+            steps += 1
+            if s1 is None:
+                w += basis.alpha * (r - basis.value(s, a, w)) * basis.feature(s, a)
+                break
+            a1 = basis.policy(s1, w, epsilon)
+            w += basis.alpha * (r + basis.value(s1, a1, w) - basis.value(s, a, w)) * basis.feature(s, a)
+            s = s1
+            a = a1
+        yield steps
+
+
+def episode_steps(episodes, epsilon, basis):
+    return np.array([steps for steps in train(episodes, epsilon, basis)])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Semi-gradient Sarsa - Tile Coding (Example 10.1).")
+    parser.add_argument("--rounds", help="total number of running rounds (default: 100)", type=int, default=100)
+    parser.add_argument("--episodes", help="number of episodes (default: 500)", type=int, default=500)
+    parser.add_argument("--epsilon", help="probability of exploration (default: 0.2)", type=float, default=0.2)
+    args = parser.parse_args()
+
+    with Pool(cpu_count()) as p:
+        plt.plot(np.mean(np.concatenate(p.map(Runner(args.episodes, args.epsilon, TileCoding(0.1)), [episode_steps for _ in range(args.rounds)])), axis=0), label="alpha=0.1")
+        plt.plot(np.mean(np.concatenate(p.map(Runner(args.episodes, args.epsilon, TileCoding(0.2)), [episode_steps for _ in range(args.rounds)])), axis=0), label="alpha=0.2")
+        plt.plot(np.mean(np.concatenate(p.map(Runner(args.episodes, args.epsilon, TileCoding(0.5)), [episode_steps for _ in range(args.rounds)])), axis=0), label="alpha=0.5")
+    plt.yscale("log")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
---
+++ utils/mountain_car.py
@@ -0,0 +1,82 @@
+import math
+import random
+import numpy as np
+
+
+class State:
+    def __init__(self, position=None, velocity=0.0):
+        if position is None:
+            self.__pos = random.uniform(-0.6, -0.4)
+        else:
+            self.__pos = position
+        self.__vel = velocity
+
+    @property
+    def position(self):
+        return self.__pos
+
+    @property
+    def velocity(self):
+        return self.__vel
+
+    def transition(self, action):
+        v = self.__vel + 0.001 * action - 0.0025 * math.cos(3 * self.__pos)
+        v = min(max(-0.07, v), 0.07)
+        p = self.__pos + v
+        if p >= 0.5:
+            return None, 0
+        elif p <= -1.2:
+            return State(-1.2), -1
+        else:
+            return State(p, v), -1
+
+
+class TileCoding:
+    def __init__(self, alpha):
+        self.__alpha = alpha / 8
+        p = np.array([
+            np.arange(9, dtype=int),
+            np.arange(1, 10, dtype=int)
+        ]) * 1.7 / 8 - 1.2
+        self.__p = np.array([p - i * 1.7 / 64 for i in range(8)])
+        v = np.array([
+            np.arange(9, dtype=int),
+            np.arange(1, 10, dtype=int)
+        ]) * 0.14 / 8 - 0.07
+        self.__v = np.array([v - i * 0.14 / 64 for i in range(8)])
+        a = np.array([
+            np.arange(9, dtype=int),
+            np.arange(1, 10, dtype=int)
+        ]) * 2.0 / 8 - 1.0
+        self.__a = np.array([a - i * 2.0 / 64 for i in range(8)])
+
+    @property
+    def alpha(self):
+        return self.__alpha
+
+    @property
+    def dimensions(self):
+        return self.__p.shape[-1] * self.__v.shape[-1] * self.__a.shape[-1] * 8
+
+    def feature(self, state, action):
+        if state is None:
+            return np.zeros((self.dimensions, 1))
+        else:
+            f = lambda x, a: np.argmax(np.logical_and(x >= a[0], x < a[1]))
+            indics = [(f(state.position, self.__p[i]), f(state.velocity, self.__v[i]), f(action, self.__a[i])) for i in range(8)]
+            features = [np.zeros((self.__p.shape[-1], self.__v.shape[-1], self.__a.shape[-1])) for i in range(8)]
+            for i, (p, v, a) in enumerate(indics):
+                features[i][p, v, a] = 1.0
+                features[i] = features[i].reshape((-1,))
+            return np.concatenate(features).reshape((-1, 1))
+
+    def value(self, state, action, weight):
+        return np.matmul(np.transpose(weight), self.feature(state, action)).item()
+
+    def policy(self, state, weight, epsilon=None):
+        actions = [-1.0, 0.0, 1.0]
+        if not epsilon is None and random.random() < epsilon:
+            return random.choice(actions)
+        else:
+            q = [self.value(state, a, weight) for a in actions]
+            return actions[np.argmax(q)]
